9f641b372ec0983bdb5eb39b36fee59a1c0a9cf7
System Administrator <root@MacBook-Pro-cua-Hieu.local>
2023-06-26 23:14:51 +0700

    initial socket

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+const app = express();
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function (socket) {
+  console.log("a user connected", socket);
+  socket.emit("connected", "User connected");
+});
+
+
+http.listen(3000, function () {
+  console.log("listening on *:3000");
+});
